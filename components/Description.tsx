aa30db7e08bc8ff19f7517d550866f4d6be6e059
apple02624 <ad.larsen.co@gmail.com>
2024-04-03 00:25:51 -0700

    fix/overflow

--- components/Description.tsx
+++ components/Description.tsx
@@ -7,7 +7,7 @@ const Description: React.FC<DescriptionProps> = ({ title, description }) => {
   return (
     <div className="flex flex-col gap-4">
       <div className="pt-10 sm:px-10 px-2 text-4xl font-bold text-gray-800">{title}</div>
-      <div className="sm:px-20 px-4 text-2xl font-mono text-gray-800">{description}</div>
+      <div className="sm:px-20 px-4 text-2xl font-mono text-gray-800 h-[100px] overflow-y-scroll">{description}</div>
     </div>
   );
 };
